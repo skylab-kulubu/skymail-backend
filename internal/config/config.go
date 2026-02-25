6a2e6db54c226189964ab0959ccc8aac801abef3
Enes Genç <hello@enesgenc.dev>
2026-02-25 18:25:17 +0300

    feat: add configuration loading and validation

---
+++ internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"errors"
+
+	"github.com/skylab-kulubu/skymail-backend/pkg/validator"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DatabaseURL string `mapstructure:"DATABASE_URL" validate:"required"`
+}
+
+func LoadConfig(vld validator.StructValidator) (config Config, err error) {
+	viper.AddConfigPath(".")
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+
+	var notFoundError viper.ConfigFileNotFoundError
+
+	if err := viper.ReadInConfig(); err != nil {
+		if !errors.Is(err, &notFoundError) {
+			return config, err
+		}
+	}
+
+	viper.SetEnvPrefix("SKYMAIL")
+	viper.AutomaticEnv()
+
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return config, err
+	}
+
+	if err = vld.Validate(&config); err != nil {
+		return config, err
+	}
+
+	return config, err
+}
